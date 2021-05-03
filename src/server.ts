32c38612e44786e7f3db69f8dc735d07fb865caa
Antonio Vuono <vuono@mswebmaker.com>
2021-05-03 14:44:48 -0300

    Created categories !

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.status(401).json({ message: "Hello World " });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running on port 3333 !"));
