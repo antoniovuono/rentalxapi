fb5b6450044f7ac0685e66bdc66280359b7d5cb4
Antonio Vuono <vuono@mswebmaker.com>
2021-05-05 22:02:37 -0300

    Specifications method create !

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import { SpecificationRepository } from "../modules/cars/repositories/SpecificationRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationRotues = Router();
+
+const specificationsRepository = new SpecificationRepository();
+
+specificationRotues.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const createSpecificationService = new CreateSpecificationService(
+    specificationsRepository
+  );
+
+  createSpecificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationRotues };
