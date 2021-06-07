cfc744f2adb82956f8adcf5ed8604834acc5f108
Antonio Vuono <vuono@mswebmaker.com>
2021-06-07 12:56:51 -0300

    Password hash implemented!

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRotues } from "./specifications.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRotues);
+router.use("/users", usersRoutes);
 
 export { router };
